1cf1dbebc090bc69b5843bd237aa4fd970f1fb83
Rohan Singh V <rohansinghv.2004@gmail.com>
2026-01-07 15:45:02 +0530

    Allow Polling Fallback

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -29,7 +29,6 @@ export const useAuthStore = create((set, get) => ({
     const socketURL = import.meta.env.MODE === "development" ? `http://${window.location.hostname}:5001` : import.meta.env.VITE_API_URL;
     const newSocket = io(socketURL, {
       query: { userId: authUser._id },
-      transports: ["websocket"],
     });
 
     set({ socket: newSocket });
